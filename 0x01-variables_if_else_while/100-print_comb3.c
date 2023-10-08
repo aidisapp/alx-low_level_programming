4c49e66682f4b30b3f9908f82d7ff3469a4f9a3b
Idongesit Ekanem <aidisapp94@gmail.com>
2023-10-08 09:58:19 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
+
 /**
- * main - main block
-  * Description: Print all possible combinations of two digits.
-   * Numbers must be separated by commas and a space.
-    * 01 and 10 are considered as the same combination of the two digits.
-	 * Print only the smallest combination of two digits.
-	  * Numbers should be printed in ascending order, with two digits.
-	   * You can only use `putchar`.
-	    * You can only use `putchar` up to 5 times.
-		 * You are not allowed to use any variable of type `char`.
-		  * Return: 0
-		   */
-		   int main(void)
-		   {
-		   	int i, j, k;
+ * main - Entry point
+ * Description: Print all possible combinations of two digits.
+ *
+ * Numbers must be separated by commas and a space.
+ *
+ * 01 and 10 are considered as the same combination of the two digits.
+ *
+ * Print only the smallest combination of two digits.
+ *
+ * Numbers should be printed in ascending order, with two digits.
+ *
+ * You can only use `putchar`.
+ *
+ * You can only use `putchar` up to 5 times.
+ *
+ * You are not allowed to use any variable of type `char`.
+ *
+ * Return: 0
+ */
 
-				i = 0;
+int main(void)
+{
+	int i, j, k;
 
-					while (i < 100)
-						{
-								j = i % 10; /* singles digit */
-										k = i / 10; /* doubles digit */
+	i = 0;
 
-												if (k < j)
-														{
-																	putchar(k + '0');
-																				putchar(j + '0');
+	while (i < 100)
+	{
+		j = i % 10; /* singles digit or numbers */
+		k = i / 10; /* doubles digit or numbers */
 
-																							if (i < 89)
-																										{
-																														putchar(44);
-																																		putchar(32);
-																																					}
-																																							}
+		if (k < j)
+		{
+			putchar(k + '0');
+			putchar(j + '0');
 
-																																									i++;
-																																										}
-																																											putchar('\n');
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);																}
+		}
 
-																																												return (0);
-																																												}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
