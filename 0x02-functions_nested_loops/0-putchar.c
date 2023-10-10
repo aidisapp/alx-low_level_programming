b4c677366fcf0c16cb59769622c3497cb1f40b2b
Idongesit Ekanem <aidisapp94@gmail.com>
2023-10-10 08:56:51 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point or main block
+ *
+ * Description: This is a program that will print _putchar
+ *
+ * Return: Always will be (0) when passed
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
