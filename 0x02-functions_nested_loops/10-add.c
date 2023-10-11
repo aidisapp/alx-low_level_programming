a52ed944574e001e23cae6283e381ae5902c9691
Idongesit Ekanem <aidisapp94@gmail.com>
2023-10-10 21:49:53 -0700

    All tasks pushes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add - this is the Entry point or the main block
+*
+* Description: this adds the two integers and returns the result
+*
+* @a: int to be added to b
+*
+* @b: int to be added to a
+*
+* Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+	int c;
+
+
+	c = a + b;
+
+
+	return (c);
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -17,5 +17,3 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 
 }
-
-
