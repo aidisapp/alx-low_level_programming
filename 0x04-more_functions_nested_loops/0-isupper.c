e121c0a9a3d28537ea7c31c1c232daa36ce95444
Idongesit Ekanem <aidisapp94@gmail.com>
2023-10-13 04:45:15 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isupper - this is the main block or the Entry point
+*
+* Description: this program is set to check for uppercase letters
+* @c: char to check
+*
+* Return: 0 or 1
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+
+	else
+	return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+#04. C - More functions, more nested loops
+This is my Readme file, and it will be indicative of all the tasks on this project
+
