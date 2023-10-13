35924cb79ab02efdc9677c05c5aed9684060d313
Idongesit Ekanem <aidisapp94@gmail.com>
2023-10-13 05:32:14 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - thi is the main block or the Entry point
+ *
+ * Description: this functions multiplies two integers
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: Always 0 on Success or Pass
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+	return (0);
+}
