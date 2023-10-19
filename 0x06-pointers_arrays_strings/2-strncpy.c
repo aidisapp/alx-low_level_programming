58d55df09b7fc8a7e7f171620f8954501141babc
Idongesit Ekanem <aidisapp94@gmail.com>
2023-10-19 13:59:37 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,11 +20,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	while (i < n)
-	{
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
 		dest[i] = '\0';
-		i++;
-	}
+
 
 	return (dest);
 }
