41f97e4ecd73cda3e37a2d067b05339926114466
Idongesit Ekanem <aidisapp94@gmail.com>
2023-10-27 18:15:58 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - this is the Entry point
+ *
+ * Description: this is the programme that prints its name,
+ *
+ * followed by a new line
+ *
+ * @argc: argument count
+ *
+ * @arcv: argument vector
+ *
+ * Return: it will certainly be 0 if passed
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+# 0x0A. C - argc, argv
+This is my readme file for this project
