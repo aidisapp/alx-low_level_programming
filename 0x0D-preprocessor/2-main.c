55831b655861b79a946179f5b30834bb0c205de4
Idongesit Ekanem <aidisapp94@gmail.com>
2023-11-08 00:53:20 +0100

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the Entry point
+ *
+ * Description: this function prints out the name of the file
+ *
+ * it was compiled from , followed by a new line
+ *
+ * Return: this is always zero at success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
