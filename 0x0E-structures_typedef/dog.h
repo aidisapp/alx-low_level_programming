5a43d6ef844adfc21c1e981970e1079052f4a7b5
Idongesit Ekanem <aidisapp94@gmail.com>
2023-11-08 02:12:36 +0100

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this is the main block
+ *
+ * Description: this is the new type with new elements
+ *
+ * @name: this is the name of the dog
+ *
+ * @age: this is representative of the age
+ *
+ * @owner: this is for the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
