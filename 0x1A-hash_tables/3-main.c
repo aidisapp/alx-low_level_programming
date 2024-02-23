ad2604f4d721530a5819016f2bc52cf659f5f1d6
Idongesit Ekanem <aidisapp94@gmail.com>
2024-02-23 11:54:34 +0100

    Checking for errors in the files

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -4,16 +4,15 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for
+ * main - check the code for ALX-Africa Cohort 5 Students.
  *
  * Return: Always EXIT_SUCCESS.
  */
-
 int main(void)
 {
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
-	printf("%p\n", (void *)ht);
+	hash_table_set(ht, "betty", "holberton");
 	return (EXIT_SUCCESS);
 }
